33f744ce3b15ce5574dc59602db36e5bcdcbe000
Santiago Cano <sanblackx1@gmail.com>
2024-05-22 16:35:47 -0400

    FIX: Types for errorElement

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,15 @@
+import SellsTable from '../components/SellsTable';
+import WeekList from '../components/WeekList';
+
 export default function Home() {
   return (
     <div>
       <section>
         <h1>Store Name</h1>
-        <div>Graph of sells</div>
+        <article>Graph of sells</article>
       </section>
+      <WeekList />
+      <SellsTable />
     </div>
   );
 }
