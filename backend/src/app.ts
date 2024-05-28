dbec7cf0aee48cd57fd4dd0d5678b3833f195b02
Santiago Cano <sanblackx1@gmail.com>
2024-05-28 00:21:22 -0400

    NEW: SQLite support

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { PORT } from './config/envLoader';
+import sellRoutes from './api/v1/routes/sellRoutes';
+import productRoutes from './api/v1/routes/productRoute';
 
 const app = express();
 app.set('x-powered-by', false);
 
 app.use(express.json());
+
+// Mount the product routes
+app.use('/api/v1', productRoutes);
+app.use('/api/v1', sellRoutes);
 
 app.use('/api/v1', (_req, res) => {
   res.json('ok');
